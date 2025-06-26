ad78b2f280ee4440530de7ea5d3460bf01044955
goodjunseon <pzs2001926@gmail.com>
2025-06-26 15:17:10 +0900

    Feat: JwtFilter, 소셜로그인 FilterChain 구축

---
+++ backend/src/main/java/com/together/backend/global/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.together.backend.global.config;
+
+import com.together.backend.global.jwt.JWTFilter;
+import com.together.backend.global.jwt.JWTUtil;
+import com.together.backend.global.oauth2.CustomOAuth2UserService;
+import com.together.backend.global.oauth2.CustomSuccessHandler;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomSuccessHandler customSuccessHandler;
+    private final JWTUtil jwtUtil;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // csrf disable
+                .csrf(auth -> auth.disable())
+                // form 로그인 방식 disable
+                .formLogin((auth)-> auth.disable())
+                //HTTP Basic 인증 disable
+                .httpBasic((auth) -> auth.disable());
+
+        //== jwt 설정 ==//
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        //== 소셜 로그인 설정 ==//
+        http
+                .oauth2Login(oauth2 -> oauth2
+                                .userInfoEndpoint(userInfo -> userInfo
+                                        .userService(customOAuth2UserService))
+                        .successHandler(customSuccessHandler)
+                        .failureHandler((request, response, exception) -> {
+                            log.error("OAuth2 로그인 실패", exception);
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "OAuth2 인증 실패: " + exception.getMessage());
+                        }));
+
+        // 경로별 인가 작업
+        http
+                .authorizeHttpRequests(auth-> auth
+                        .requestMatchers ("/", "/oauth2/**", "/public/**").permitAll()
+                        .anyRequest().authenticated() // 인증이 필요할 땐 자동으로 oauth2Login() 실행
+                );
+
+        // 세션 설정 : STATELESS
+        http
+                .sessionManagement((session) ->session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
