a1e97496cd14d495d73c4f46726ceaf5c0a2e7e4
goodjunseon <pzs2001926@gmail.com>
2025-06-29 01:06:05 +0900

    Rename: 폴더 및 클래스명 변경

--- backend/src/main/java/com/together/backend/user/repository/UserRepository.java
+++ backend/src/main/java/com/together/backend/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.together.backend.user.repository;
 
-import com.together.backend.user.entity.User;
+import com.together.backend.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
