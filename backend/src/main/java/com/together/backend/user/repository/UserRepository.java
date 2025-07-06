df232bf95c8a4052c6f8543fb9e7ce9061e56a25
goodjunseon <pzs2001926@gmail.com>
2025-07-06 17:19:07 +0900

    Refactor: findByEmail() 반환값 Optional 객체 변경

--- backend/src/main/java/com/together/backend/user/repository/UserRepository.java
+++ backend/src/main/java/com/together/backend/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.together.backend.user.repository;
 import com.together.backend.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
