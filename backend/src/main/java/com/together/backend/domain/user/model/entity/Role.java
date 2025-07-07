3c434ef7dd0a9bffa43017496facfe8b3a71670e
goodjunseon <pzs2001926@gmail.com>
2025-07-07 17:58:53 +0900

    Feat: role 값 추가: ROLE_PARTNER

--- backend/src/main/java/com/together/backend/domain/user/model/entity/Role.java
+++ backend/src/main/java/com/together/backend/domain/user/model/entity/Role.java
@@ -1,6 +1,6 @@
 package com.together.backend.domain.user.model.entity;
 
 public enum Role {
-    ROLE_USER
+    ROLE_USER, ROLE_PARTNER; // 현재는 ROLE_USER, ROLE_PARTNER만 사용
     // 추후 개발 시 ROLE_ADMIN 등 추가 가능
 }
