463f63113e9d307295fc98bd1e3b43d46589a5e8
Lims <swoo0514lim@gmail.com>
2025-07-08 22:08:41 +0900

    Feat: dto 생성

---
+++ backend/src/main/java/com/together/backend/domain/notification/model/notification/response/NotificationEnabledResponse.java
@@ -0,0 +1,12 @@
+package com.together.backend.domain.notification.model.notification.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationEnabledResponse {
+    private Boolean enabled;
+}
---
+++ backend/src/main/java/com/together/backend/domain/notification/model/notification/response/NotificationTimeResponse.java
@@ -0,0 +1,13 @@
+package com.together.backend.domain.notification.model.notification.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationTimeResponse {
+    private String time;
+    private Boolean enabled;
+}
