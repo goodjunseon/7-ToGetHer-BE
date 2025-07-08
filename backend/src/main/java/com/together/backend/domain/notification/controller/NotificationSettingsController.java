b5c7edcb588495cfb1f8bdd4a56acfd8795c0cc1
goodjunseon <pzs2001926@gmail.com>
2025-07-08 17:57:08 +0900

    Feat: 복용 알림 시간 중복 저장 예외 처리 로직

--- backend/src/main/java/com/together/backend/domain/notification/controller/NotificationSettingsController.java
+++ backend/src/main/java/com/together/backend/domain/notification/controller/NotificationSettingsController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,52 +23,54 @@ public class NotificationSettingsController {
 
     // 피임약 복용 알림 시간 저장
     @PostMapping("/notification-settings/pill-intake-time")
-    public BaseResponse<IntakeResponse> savePillIntakeTime(@AuthenticationPrincipal CustomOAuth2User oAuth2User,
-                                                           @RequestBody IntakeRequest dto) {
+    public BaseResponse<IntakeResponse> savePillIntakeTime(@AuthenticationPrincipal CustomOAuth2User oAuth2User, @RequestBody IntakeRequest request) {
         // oAuth2User가 null인 경우 처리
         if (oAuth2User == null) {
             return new BaseResponse<>(BaseResponseStatus.UNAUTHORIZED, null);
         }
+
         String email = oAuth2User.getEmail(); // 클라이언트 이메일 추출
         log.info("savePillIntakeTime() 호출됨, 사용자 이메일: {}", email);
 
-
-        LocalTime intakeTime;
         try {
-            // 알림 시간 저장 로직 (예: 데이터베이스에 저장)
-            intakeTime = LocalTime.parse(dto.getIntakeTime());
+            LocalTime intakeTime = LocalTime.parse(request.getIntakeTime());
             log.info("사용자 {}의 피임약 복용 알림 시간 저장: {}", email, intakeTime);
+            IntakeResponse data = notificationIntakeService.createPillIntakeTime(email, intakeTime);
+            return new BaseResponse<>(BaseResponseStatus.OK, data);
+        } catch (IllegalArgumentException e) {
+            log.error("알림 중복 설정:");
+            return new BaseResponse<>(BaseResponseStatus.DUPLICATE);
+        } catch (DateTimeParseException e) {
+            log.error("시간 파싱 오류: {}" , request.getIntakeTime(), e);
+            return new BaseResponse<>(BaseResponseStatus.BAD_REQUEST);
         } catch (Exception e){
-            log.error("잘못된 시간 형식: {}", dto.getIntakeTime(), e);
-            return new BaseResponse<>(BaseResponseStatus.BAD_REQUEST, null);
+            log.error("알 수 없는 서버 오류 발생", e);
+            return new BaseResponse<>(BaseResponseStatus.INTERNAL_SERVER_ERROR);
         }
-
-        IntakeResponse data = notificationIntakeService.createPillIntakeTime(email, intakeTime);
-        return new BaseResponse<>(BaseResponseStatus.OK, data);
-
     }
 
     @PatchMapping("/notification-settings/pill-intake-time")
     public BaseResponse<IntakeResponse> updatePillIntakeTime(@AuthenticationPrincipal CustomOAuth2User oAuth2User,
-                                                             @RequestBody IntakeRequest dto) {
+                                                             @RequestBody IntakeRequest request) {
         if (oAuth2User == null) {
             return new BaseResponse<>(BaseResponseStatus.UNAUTHORIZED, null);
         }
         String email = oAuth2User.getEmail(); // 클라이언트 이메일 추출
         log.info("updatePillIntakeTime() 호출됨, 사용자 이메일: {}", email);
 
-        // 알림 시간 업데이트 로직 (예: 데이터베이스에 저장)
-        LocalTime intakeTime;
-
         try {
-            intakeTime = LocalTime.parse(dto.getIntakeTime());
+            LocalTime intakeTime = LocalTime.parse(request.getIntakeTime());
             log.info("사용자 {}의 피임약 복용 알림 시간 업데이트: {}", email, intakeTime);
+            IntakeResponse data = notificationIntakeService.updatePillIntakeTime(email, intakeTime);
+            return new BaseResponse<>(BaseResponseStatus.OK, data);
+        } catch (DateTimeParseException e) {
+            log.error("시간 파싱 오류: {}" , request.getIntakeTime(), e);
+            return new BaseResponse<>(BaseResponseStatus.BAD_REQUEST);
         } catch (Exception e) {
-            log.error("잘못된 시간 형식: {}", dto.getIntakeTime(), e);
-            return new BaseResponse<>(BaseResponseStatus.BAD_REQUEST, null);
+            log.error("알 수 없는 서버 오류 발생", e);
+            return new BaseResponse<>(BaseResponseStatus.INTERNAL_SERVER_ERROR);
         }
 
-        IntakeResponse data = notificationIntakeService.updatePillIntakeTime(email, intakeTime);
-        return new BaseResponse<>(BaseResponseStatus.OK, data);
+
     }
 }
