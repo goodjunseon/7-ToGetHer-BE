f4c09b19f46ad7a608240cfb163d219a6ce2399f
Lims <swoo0514lim@gmail.com>
2025-07-08 22:19:11 +0900

    Feat: service 레이어 upsert 형식으로 수정 및 타입 분기

--- backend/src/main/java/com/together/backend/domain/notification/model/notification/response/NotificationTimeResponse.java
+++ backend/src/main/java/com/together/backend/domain/notification/model/notification/response/NotificationTimeResponse.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class NotificationTimeResponse {
-    private String time;
     private Boolean enabled;
+    private String time;
 }
--- backend/src/main/java/com/together/backend/domain/notification/service/NotificationIntakeService.java
+++ backend/src/main/java/com/together/backend/domain/notification/service/NotificationIntakeService.java
@@ -3,6 +3,7 @@ package com.together.backend.domain.notification.service;
 import com.together.backend.domain.notification.model.NotificationSettings;
 import com.together.backend.domain.notification.model.NotificationType;
 import com.together.backend.domain.notification.model.intake.response.IntakeResponse;
+import com.together.backend.domain.notification.model.notification.response.NotificationTimeResponse;
 import com.together.backend.domain.notification.repository.NotificationSettingsRepository;
 import com.together.backend.domain.user.model.entity.User;
 import com.together.backend.domain.user.repository.UserRepository;
@@ -17,41 +18,32 @@ public class NotificationIntakeService {
     private final NotificationSettingsRepository notificationSettingsRepository;
     private final UserRepository userRepository;
 
-    public IntakeResponse createPillIntakeTime(String email, LocalTime intakeTime) {
+    public NotificationTimeResponse upsertNotificationTime(String email, NotificationType type, LocalTime time) {
 
         // 이메일로 사용자 조회
         User user = userRepository.findByEmail(email)
                 .orElseThrow(()-> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
 
-        boolean exists = notificationSettingsRepository.findByUserAndType(user, NotificationType.PILL_INTAKE).isPresent();
+        // NotificationSetting 엔티티를 user와 NotificationType으로 조회
+        NotificationSettings setting = notificationSettingsRepository.findByUserAndType(user, type)
+                .orElse(null);
 
-        if (exists) {
-            throw new IllegalArgumentException("이미 알림 설정이 존재합니다.");
+        if(setting == null) {
+            // INSERT
+            setting = NotificationSettings.builder()
+                    .user(user)
+                    .type(NotificationType.PILL_INTAKE)
+                    .isEnabled(true)
+                    .notificationTime(time)
+                    .build();
+        } else {
+            // UPDATE
+            setting.setNotificationTime(time);
         }
 
-        // NotificationSetting 엔티티를 user와 NotificationType으로 조회
-        NotificationSettings setting = NotificationSettings.builder()
-                .user(user)
-                .type(NotificationType.PILL_INTAKE)
-                .isEnabled(true)
-                .notificationTime(intakeTime)
-                .build();
-
         notificationSettingsRepository.save(setting);
 
-        return new IntakeResponse(setting.isEnabled(), setting.getNotificationTime().toString());
-    }
-
-    public IntakeResponse updatePillIntakeTime(String email, LocalTime intakeTime) {
-        // 이메일로 사용자 조회
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
-        NotificationSettings setting = notificationSettingsRepository.findByUserAndType(user,NotificationType.PILL_INTAKE)
-                .orElseThrow(() -> new IllegalArgumentException("알림 설정이 존재하지 않습니다."));
-
-        setting.setNotificationTime(intakeTime);
-        notificationSettingsRepository.save(setting);
-        return new IntakeResponse(setting.isEnabled(), setting.getNotificationTime().toString());
+        return new NotificationTimeResponse(setting.isEnabled(), setting.getNotificationTime().toString());
     }
 
 }
