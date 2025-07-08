9cb1aeeedbc789e597ce414de3ed358785fb9f36
Lims <swoo0514lim@gmail.com>
2025-07-09 00:14:36 +0900

    Feat: pill-purchase 타입 살리기

--- backend/src/main/java/com/together/backend/domain/notification/model/NotificationType.java
+++ backend/src/main/java/com/together/backend/domain/notification/model/NotificationType.java
@@ -2,7 +2,7 @@ package com.together.backend.domain.notification.model;
 
 public enum NotificationType {
     // 알림 타입 정의
-    // PILL_PURCHASE, // 약 구매 알림 - 기능 제거 되었습니다
+     PILL_PURCHASE("pill-purchase"), // 약 구매 알림
     PILL_INTAKE("pill-intake"), // 약 복용 알림
     PARTNER_REQUEST("partner-request"), // 파트너 요청
     EMOTION_UPDATE("emotion-update"); // 감정 업데이트
