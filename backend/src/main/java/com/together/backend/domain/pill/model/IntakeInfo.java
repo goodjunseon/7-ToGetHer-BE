98be8ce4f8d73d0fd2546e15d101ba4d096a110e
Lims <swoo0514lim@gmail.com>
2025-07-09 02:12:55 +0900

    IntakeInfo 필드 추가

--- backend/src/main/java/com/together/backend/domain/pill/model/IntakeInfo.java
+++ backend/src/main/java/com/together/backend/domain/pill/model/IntakeInfo.java
@@ -3,6 +3,8 @@ package com.together.backend.domain.pill.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Setter
@@ -23,4 +25,31 @@ public class IntakeInfo {
     @Column(name = "type", nullable = false)
     @Enumerated(EnumType.STRING)
     private IntakeOption option; // 복용 방법
+
+    @Column(name = "name", length = 20)
+    private String name;
+
+    @Column(name = "real_days")
+    private Integer realDays;
+
+    @Column(name = "fake_days")
+    private Integer fakeDays;
+
+    @Column(name = "break_days")
+    private Integer breakDays;
+
+    @Column(name = "is_fake")
+    private Boolean isFake;
+
+    @Column(name = "is_break")
+    private Boolean isBreak;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "prescription_amount")
+    private Integer prescriptionAmount;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 }
