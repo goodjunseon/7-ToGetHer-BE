57b364480961bd2f1341b379cd89a4f16727b877
goodjunseon <pzs2001926@gmail.com>
2025-07-09 12:46:57 +0900

    Feat: "피임약 복용 N일차" 응답 DTO

---
+++ backend/src/main/java/com/together/backend/domain/user/model/response/mainpageinfo/PillInfoResponse.java
@@ -0,0 +1,10 @@
+package com.together.backend.domain.user.model.response.mainpageinfo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PillInfoResponse {
+    private long daysOnPill;
+}
