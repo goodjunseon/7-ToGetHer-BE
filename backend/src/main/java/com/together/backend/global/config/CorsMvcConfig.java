a36a72cb196663a3856a12d18a86b7b5c93fa061
goodjunseon <pzs2001926@gmail.com>
2025-07-12 23:53:09 +0900

    HotFix: cors설정 변경

--- backend/src/main/java/com/together/backend/global/config/CorsMvcConfig.java
+++ backend/src/main/java/com/together/backend/global/config/CorsMvcConfig.java
@@ -1,21 +1,21 @@
-package com.together.backend.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsMvcConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:3000",
-                        "https://7-together.kro.kr" // 개발용
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .exposedHeaders("Set- Cookie")
-                .allowCredentials(true);
-    }
-}
+//package com.together.backend.global.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsMvcConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry corsRegistry) {
+//        corsRegistry.addMapping("/**")
+//                .allowedOrigins(
+//                        "http://localhost:3000",
+//                        "https://7-together.kro.kr" // 개발용
+//                )
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .exposedHeaders("Set- Cookie")
+//                .allowCredentials(true);
+//    }
+//}
